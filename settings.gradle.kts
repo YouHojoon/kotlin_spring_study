be73b5fa407dabb112ce4d45488e5254954635d2
YouHojoon <dbghwns66@daum.net>
2023-09-04 15:14:39 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "prlab_server"
---
+++ src/main/kotlin/ac/kr/smu/prlab_server/PrlabServerApplication.kt
@@ -0,0 +1,11 @@
+package ac.kr.smu.prlab_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrlabServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrlabServerApplication>(*args)
+}
