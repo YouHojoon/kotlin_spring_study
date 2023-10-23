83302c8f5b73a99518481b84c26119a3980dbd53
YouHojoon <dbghwns66@daum.net>
2023-10-24 01:14:33 +0900

    add: main_server Dockerfile

--- main_server/src/main/kotlin/ac/kr/smu/prlab_server/PrlabServerApplication.kt
+++ main_server/src/main/kotlin/ac/kr/smu/prlab_server/PrlabServerApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 class PrlabServerApplication
 
 fun main(args: Array<String>) {
