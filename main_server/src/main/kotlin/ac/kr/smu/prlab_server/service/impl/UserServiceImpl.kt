417a009dbdaf16fdede19f6687ade770ede30783
YouHojoon <dbghwns66@daum.net>
2024-04-13 00:03:53 +0900

    fix: 측정 데이터 cascade 속성 활성화

--- main_server/src/main/kotlin/ac/kr/smu/prlab_server/service/impl/UserServiceImpl.kt
+++ main_server/src/main/kotlin/ac/kr/smu/prlab_server/service/impl/UserServiceImpl.kt
@@ -36,8 +36,7 @@ class UserServiceImpl(
     override fun findById(id: String): User? {
         return repo.findById(id).getOrNull()
     }
-
-    @Transactional(readOnly = true)
+    @Transactional
     override fun deleteUser(id:String){
         repo.deleteUserById(id)
     }
